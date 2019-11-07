92c71f54f25157cfac1a1a0abcd98af07b8b0742
unknown <marik.3000@hotmail.com>
2019-11-07 14:04:40 +0100

    PRO,CONS

---
+++ Consumer/src/main/java/com/consumer/demo/RabbitApplication.java
@@ -0,0 +1,84 @@
+package com.consumer.demo;
+
+import com.rabbitmq.client.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class RabbitApplication
+{
+    private final static String QUEUE_NAME = "hello";
+    public static void main(String[] args) throws Exception {
+
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+    }
+
+
+
+}
+
+    /*
+    private final static String QUEUE_Car = "carqueue";
+
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(RabbitApplication.class, args);
+        Scanner sc = new Scanner(System.in);
+        while(true){
+        String msg = sc.next();
+        connectQueue(msg);}
+    }
+
+    public static void connectQueue(String msg) throws Exception
+    {
+        // Same as the producer: tries to create a queue, if it wasn't already created
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        try
+        {
+            channel.queueDeclare(QUEUE_Car, false, false, false, null);
+            channel.basicPublish("", QUEUE_Car, null, msg.getBytes("UTF-8"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Register for a queue
+        channel.queueDeclare(QUEUE_Car, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Get notified, if a message for this receiver arrives
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->
+        {
+            String message = new String(delivery.getBody(), "UTF-8");
+            if(message.equals("1")) {
+                System.out.println("wait until the producer connect");
+
+            }
+            if(!message.equals("1")) {
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_Car, true, deliverCallback, consumerTag -> {});
+
+
+
+    }
+     */
---
+++ Producer/src/main/java/com/example/demo/RabbitApplication.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+@SpringBootApplication
+public class RabbitApplication {
+    private final static String QUEUE_NAME = "hello";
+
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello she!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+
+    }
+}
+    /*
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RabbitApplication.class, args);
+
+        Scanner sc = new Scanner(System.in);
+
+        String message = "Hello, Welcome to Hallur&Mo car rent, please pick a car that you would like to rent:" +
+                "\n1-Mercdes CLA200 Benz," +
+                " \n2-BMW i530 Benz!," +
+                "\n3-Toyota Aygo 2010-benz, " +
+                "\n3-mercedes E200 2015-Ben";
+        createQueue(message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+
+    public static void createQueue(String message) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        try
+        {
+            channel.queueDeclare(QUEUE_Car, false, false, false, null);
+            channel.basicPublish("", QUEUE_Car, null, message.getBytes("UTF-8"));
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        channel.queueDeclare(QUEUE_Car, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        // Get notified, if a message for this receiver arrives
+        DeliverCallback deliverCallback = (consumerTag, delivery) ->
+        {
+            String message2 = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message2 + "'");
+            if(message2.equals("1")){
+                String msg = "you order mercdes";
+                channel.basicPublish("", QUEUE_Car, null, msg.getBytes("UTF-8"));
+            }
+        };
+        channel.basicConsume(QUEUE_Car, true, deliverCallback, consumerTag -> {});
+    }
+
+     */
+
